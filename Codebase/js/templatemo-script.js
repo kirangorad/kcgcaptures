884d17853daf0f2f7ca3e82673868b791a3e297f
kirangorad <kiran.gorad@yahoo.in>
2023-03-05 16:22:34 +0100

    initial commit

---
+++ Codebase/js/templatemo-script.js
@@ -0,0 +1,87 @@
+const initBg = (autoplay = true) => {
+    const bgImgsNames = [
+        "DJI_0093.webmhd.webm",
+        "https://www.youtube.com/embed/KU-rCyD2LvA", 
+        'diagoona-bg-2.jpg', 
+        'diagoona-bg-3.jpg'
+    ];
+    const bgImgs = bgImgsNames.map(img => "img/" + img);
+
+    $.backstretch(bgImgs, {duration: 4000, fade: 500});
+
+    if(!autoplay) {
+      $.backstretch('pause');  
+    }    
+}
+
+const setBg = id => {
+    $.backstretch('show', id);
+}
+
+const setBgOverlay = () => {
+    const windowWidth = window.innerWidth;
+    const bgHeight = $('body').height();
+    const tmBgLeft = $('.tm-bg-left');
+
+    $('.tm-bg').height(bgHeight);
+
+    if(windowWidth > 768) {
+        tmBgLeft.css('border-left', `0`)
+                .css('border-top', `${bgHeight}px solid transparent`);                
+    } else {
+        tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
+                .css('border-top', `0`);
+    }
+}
+
+$(document).ready(function () {
+    const autoplayBg = true;	// set Auto Play for Background Images
+    initBg(autoplayBg);    
+    setBgOverlay();
+
+    const bgControl = $('.tm-bg-control');            
+    bgControl.click(function() {
+        bgControl.removeClass('active');
+        $(this).addClass('active');
+        const id = $(this).data('id');                
+        setBg(id);
+    });
+
+    $(window).on("backstretch.after", function (e, instance, index) {        
+        const bgControl = $('.tm-bg-control');
+        bgControl.removeClass('active');
+        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);        
+        current.addClass('active');
+    });
+
+    $(window).resize(function() {
+        setBgOverlay();
+    });
+
+    
+    var $container = $('.filterable-items');
+
+    
+    $('.filterable-nav a').click(function(e){
+        e.preventDefault();
+        $('.filterable-nav .current').removeClass('current');
+        $(this).addClass('current');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+            });
+            return false;
+    });
+    
+});
+
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
